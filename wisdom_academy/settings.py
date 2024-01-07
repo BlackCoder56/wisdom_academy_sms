9eeded998eda8c9dc399024263b88f4b1a767ef7
BlackCoder56 <blackcoda56@gmail.com>
2024-01-07 15:15:52 +0300

    lx v

--- wisdom_academy/settings.py
+++ wisdom_academy/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sms',
         'USER': 'postgres',
-        'PASSWORD': 'swap1',
+        'PASSWORD': 'swap2',
         'HOST': 'localhost',
         'PORT': '5432',
     }
